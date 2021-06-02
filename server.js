3f5a02aa8826076c68990e3771059c16dec15d11
VictorV031 <69357636+victorv031@users.noreply.github.com>
2021-06-01 22:08:44 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-/**HELLO WORD */
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res)=>{
+    // req, quer dizer request de requisição
+    // res quer dizer response de resposta
+    res.send('Ola mundo')
+})
+
+//Para iniciar, digite -> node server.js
+app.listen(8080, ()=>{
+    console.log('Servidor online')
+})
